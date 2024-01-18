02e1d9ffee300e0e0da825b2cb3fe09ff935b473
Chetan kaushik <chetankaushik29@gmail.com>
2024-01-18 23:09:46 +0530

    Stop watch is created with animation

---
+++ script.js
@@ -0,0 +1,35 @@
+let milSec = 0;
+let timer;
+
+document.getElementById("start").addEventListener("click", startTimer);
+ document.getElementById("pause").addEventListener("click",pauseTimer);
+ document.getElementById("reset").addEventListener("click",resetTimer);
+
+ function resetTimer(){
+    clearInterval(timer);
+    milSec=0;
+    updateTime(milSec);
+    document.getElementById("miliSec").innerText =0+"0";
+ }
+
+function pauseTimer(){
+    clearInterval(timer);
+}
+
+function startTimer() {
+    timer = setInterval(() => {
+        milSec += 10;
+        updateTime(milSec)
+    }, 10)
+    
+}
+
+function updateTime(miliSec) {
+    const sec = Math.floor(miliSec / 1000);
+    const min = Math.floor(miliSec / 60000);
+    const ms = (miliSec % 1000) / 10;
+
+    document.getElementById("miliSec").innerText =ms;
+    document.getElementById("sec").innerText = sec<10 ?"0"+ sec%60 : sec%60;
+    document.getElementById("min").innerText = min<10 ? "0"+min%60:min%60;
+}
